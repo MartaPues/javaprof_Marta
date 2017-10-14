7dd4b047e2b8897aefe2e72a03ef7fc0e0599bfe
Marta <martam@gmail.com>
2017-10-14 19:06:49 +0200

    version1

---
+++ 02_saludo_aumento/src/servlets/SaludoAumento.java
@@ -0,0 +1,31 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/SaludoAumento")
+public class SaludoAumento extends HttpServlet {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		out.println("<html><body><center>");
+		for(int i=6;i>=1;i--) {
+			out.println("<h"+i+">Bienvenido a mi servlet</h"+i+">");
+		}
+		out.println("</center></body></html>");
+	}
+
+}
